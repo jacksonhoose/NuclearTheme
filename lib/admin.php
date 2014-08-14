4255b7c4cd9f112e7342247f036fab3f39b05847
Jackson Hoose <jackson.hoose@commonmedia.com>
2014-08-14 12:35:10 -0400

    fixed formatting

--- lib/admin.php
+++ lib/admin.php
@@ -2,13 +2,13 @@
 
 function nuclear_cleanup_dashboard()
 {
-	remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'core' );
-	remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'core' );
-	remove_meta_box( 'dashboard_plugins', 'dashboard', 'core' );
-	remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'core' );
-	remove_meta_box( 'dashboard_primary', 'dashboard', 'core' );
-	remove_meta_box( 'dashboard_secondary', 'dashboard', 'core' );
-	remove_meta_box( 'yoast_db_widget', 'dashboard', 'normal' );
+	remove_meta_box('dashboard_recent_comments', 'dashboard', 'core');
+	remove_meta_box('dashboard_incoming_links', 'dashboard', 'core');
+	remove_meta_box('dashboard_plugins', 'dashboard', 'core');
+	remove_meta_box('dashboard_recent_drafts', 'dashboard', 'core');
+	remove_meta_box('dashboard_primary', 'dashboard', 'core');
+	remove_meta_box('dashboard_secondary', 'dashboard', 'core');
+	remove_meta_box('yoast_db_widget', 'dashboard', 'normal');
 }
 
 add_action( 'wp_dashboard_setup', 'nuclear_cleanup_dashboard' );
