685ddd3ba486ef80427e90c65c848d033eddb207
Jackson Hoose <jackson.hoose@commonmedia.com>
2014-08-14 12:06:45 -0400

    removed fake post type

--- lib/init.php
+++ lib/init.php
@@ -46,39 +46,7 @@ function nuclear_register_menus()
 
 function nuclear_post_types()
 {
-	$labels = [
-		'name'               => 'Clients',
-		'singular_name'      => 'Client',
-		'menu_name'          => _x('Clients', 'admin menu', 'your-plugin-textdomain'),
-		'name_admin_bar'     => _x('Client', 'add new on admin bar', 'your-plugin-textdomain'),
-		'add_new'            => _x('Add New', 'client', 'your-plugin-textdomain'),
-		'add_new_item'       => __('Add New Client', 'your-plugin-textdomain'),
-		'new_item'           => __('New Client', 'your-plugin-textdomain'),
-		'edit_item'          => __('Edit Client', 'your-plugin-textdomain'),
-		'view_item'          => __('View Client', 'your-plugin-textdomain'),
-		'all_items'          => __('All Clients', 'your-plugin-textdomain'),
-		'search_items'       => __('Search Clients', 'your-plugin-textdomain'),
-		'parent_item_colon'  => __('Parent Clients:', 'your-plugin-textdomain'),
-		'not_found'          => __('No clients found.', 'your-plugin-textdomain'),
-		'not_found_in_trash' => __('No clients found in Trash.', 'your-plugin-textdomain')
-	];
-
-	$args = [
-		'labels'             => $labels,
-		'public'             => true,
-		'publicly_queryable' => true,
-		'show_ui'            => true,
-		'show_in_menu'       => true,
-		'query_var'          => true,
-		'rewrite'            => ['slug' => 'client'],
-		'capability_type'    => 'post',
-		'has_archive'        => false,
-		'hierarchical'       => false,
-		'menu_position'      => null,
-		'supports'           => ['title']
-	];
-
-	register_post_type('client', $args);
+	
 }
 
 function nuclear_cleanup_head()
