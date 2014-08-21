c98cd5c1869f29e8c4befb51aaa25a524d621e5c
Jackson Hoose <jackson.hoose@commonmedia.com>
2014-08-21 11:09:57 -0400

    fixed format.php html

--- templates/post-formats/format.php
+++ templates/post-formats/format.php
@@ -1,16 +1,18 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class('cf entry'); ?> role="article" itemscope>
+<article id="post-<?php the_ID(); ?>" <?php post_class('cf entry'); ?> role="article">
 
 	<header class="article-header">
-		<h1 class="article-title single-title" itemprop="headline">
+		<h1 class="article-title single-title">
 			<a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark">
 				<?php the_title(); ?>
 			</a>
 		</h1>
-		<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time>
+		<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate>
+			<?php the_date(); ?> <?php the_time(); ?>
+		</time>
 		<?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
 	</header>
 
-	<section itemprop="articleBody" class="cf article-content">
+	<section class="cf article-content">
 		<?php the_content(); ?>
 		<?php wp_link_pages([
 				'before'      => '<div class="page-links"><span class="page-links-title">' . __('Pages:') . '</span>',
@@ -29,7 +31,5 @@
 	<?php endif; ?>
 
 	<footer class="article-footer"></footer>
-
-	<?php comments_template(); ?>
 
 </article>
